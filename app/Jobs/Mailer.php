213f395c4e7c0496d1cc3a52f8c57ef9033deba0
adlet29 <adletassanov26@gmail.com>
2022-01-03 19:47:39 +0600

    test queue

--- app/Jobs/Mailer.php
+++ app/Jobs/Mailer.php
@@ -13,14 +13,15 @@ class Mailer implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $message;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($message)
     {
-        //
+        $this->message = $message;
     }
 
     /**
@@ -30,6 +31,6 @@ class Mailer implements ShouldQueue
      */
     public function handle()
     {
-        //
+        info($this->message);
     }
 }
